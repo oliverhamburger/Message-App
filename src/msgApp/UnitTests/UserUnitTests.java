4b1923c18cc645014e0581153306258418365b8f
Oliver Hamburger <1169488@us.ray.com>
2023-03-28 11:33:32 -0600

    build.xml updated and unit tests started

---
+++ src/msgApp/UnitTests/UserUnitTests.java
@@ -0,0 +1,14 @@
+package msgApp.UnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserUnitTests {
+
+	@Test
+	void test() {
+		assertEquals(true,true);
+	}
+
+}
